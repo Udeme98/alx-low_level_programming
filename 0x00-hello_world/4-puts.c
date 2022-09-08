a77ad03aaec25b339698b88056a661d7dbe0c92a
Udeme98 <darealudeme1@gmail.com>
2022-09-08 11:43:15 -0700

    Script to print with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * We used function put to pint out the word
+ * main - prints Programming is like building a multilingual puzzle
+ * The value ands with 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building is like building a Multilingual puzzle");
+	return(0);
+
+}
