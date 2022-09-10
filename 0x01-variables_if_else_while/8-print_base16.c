20cfeedbe471ee52f2c635773c83a7f90110cfbf
Udeme98 <darealudeme1@gmail.com>
2022-09-10 08:00:41 -0700

    Prints numbers in base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,10 +8,15 @@
 int main(void)
 {
 	int n;
+	char ch;
 
-	for (n = 0; n <= 'f'; n++)
+	for (n = 0; n <= 9; n++)
 	{
 		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch)
 	}
 	putchar('\n');
 	return (0);
