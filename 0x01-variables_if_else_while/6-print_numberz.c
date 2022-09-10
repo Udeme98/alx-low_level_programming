f6430f68ba2fe7028f962c1bbde3abac5644632d
Udeme98 <darealudeme1@gmail.com>
2022-09-10 04:56:09 -0700

    Prints base 10 digit with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints
+ * numbers using putchar
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+}
