61c02706112cca7510963d8f1e49a527da67487e
Udeme98 <darealudeme1@gmail.com>
2022-09-10 03:38:32 -0700

    To print both upper and lower case alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -prints
+ * both upper and lowcase alphabet
+ * Return: Alays 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
