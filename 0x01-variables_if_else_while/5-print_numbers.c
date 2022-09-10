4335676690a0adb341b07f8a7acc7376347a6b95
Udeme98 <darealudeme1@gmail.com>
2022-09-10 04:21:03 -0700

    Prints digits of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints
+ * numbers of base
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 10; n++)
+	{
+		printf("%d\n", n);
+	}
+	return (0);
+}
