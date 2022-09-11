98e26217eff7a21deb442c74f401ae1c33974131
Udeme98 <darealudeme1@gmail.com>
2022-09-11 13:56:26 -0700

    prints 3 digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,7 +20,7 @@ int main(void)
 						putchar(i);
 						putchar(j);
 						putchar(k);
-						if (i != 55 && j == 56)
+						if (i != 55 && j != 56)
 						{
 							putchar(',');
 							putchar(' ');
