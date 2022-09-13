2809209f85c2d0df1f1400a60e5feb8e0521743f
Udeme98 <darealudeme1@gmail.com>
2022-09-13 11:44:30 -0700

    Prints lowercase 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowercase
+ * alphabet followed by new line
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char n;
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+	}
+	_puchar('\n');
+}
