e55c97891955a98ca691bd3d590e7df945c3e3ab
Udeme98 <darealudeme1@gmail.com>
2022-09-13 06:13:03 -0700

    prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar
+ * followed by a new line
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+	putchar("_putchar");
+	putchar('\n');
+	return (0);
+}
