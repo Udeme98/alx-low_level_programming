4328567a0edd8e0ca4c430e667b0ebe2827dd58f
Udeme98 <darealudeme1@gmail.com>
2022-09-13 15:12:24 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,19 +14,23 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			k = i * j;
-			if (k <= 9)
+			if (j == 0)
 			{
 				_putchar(k + '0');
 			}
-			if (k > 9)
-			{
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-			}
-			if (j != 9)
+			if (k < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 		}
 		_putchar('\n');
