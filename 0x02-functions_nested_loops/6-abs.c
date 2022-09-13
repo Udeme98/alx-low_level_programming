f9bac4bb48b0b4d20fab83443721e17c2791367b
Udeme98 <darealudeme1@gmail.com>
2022-09-13 13:12:45 -0700

    Commutes integer value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute
+ * absolute value of an integer
+ * @n: the integert to check
+ * Return: 0
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
