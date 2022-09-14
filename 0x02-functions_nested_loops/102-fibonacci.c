c0df2026f665827330a366d0a323dae041a13187
Udeme98 <darealudeme1@gmail.com>
2022-09-14 02:19:46 -0700

    102 fibonacci task

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints the ist 50 Fibonacci numbers starting with 1 and 2
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+	long int i, j, k;
+
+	j = 1;
+
+	k = 2;
+
+	for (i = 1; i <= 50; ++i)
+	{
+		if (j != 20365011074)
+		{
+			printf("%ld, ", j);
+		} else
+		{
+			printf("%ld\n", j);
+		}
+		next = j + k;
+		j = k;
+		k = next;
+	}
+	return (0);
+}
