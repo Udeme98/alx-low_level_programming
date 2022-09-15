e71c015c918c5c86ac5111146be0edbe1c4596d1
Udeme98 <darealudeme1@gmail.com>
2022-09-15 05:27:12 -0700

    Checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isUpper - checks for uppercase character
+ * @c: character to check
+ * Return: 1 uppercase and 0 otherwise
+ */
+int _isUpper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
