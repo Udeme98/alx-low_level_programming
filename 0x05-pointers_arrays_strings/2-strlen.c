de2b4d0aa6269c5d9197350a0d1d02a006868e28
Udeme98 <darealudeme1@gmail.com>
2022-09-19 05:31:21 -0700

    Prints the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns th length of a string
+ * @s: string to find it's length
+ * Return: void
+ */
+int _strlen(char *s)
+{
+	int = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
