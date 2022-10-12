ee499e0502c1718d1d983e564b1a9f63e9ecb351
Udeme98 <darealudeme1@gmail.com>
2022-10-12 09:25:27 -0700

    Prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * print_name - prints a name
@@ -9,5 +10,5 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name)
+	printf("%s\n", name);
 }
