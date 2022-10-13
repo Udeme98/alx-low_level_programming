55df2ba423e3d224dae623c6d544f692ad9bc00a
Udeme98 <darealudeme1@gmail.com>
2022-10-13 05:10:09 -0700

    Simple Arithmetic operation

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two integers
+ * @a: first int
+ * @b: second int
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - finds the differences between two numbers
+ * @a: first integer
+ * @b: second integer
+ * Return: the difference between a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - calculates the division of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the division of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - calculates the remainder of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the remainer of a / b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
